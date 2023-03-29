3c7962a65af81c13ed7bbc63f51bcfd646064dcd
fatimazyani <123373140+fatimazyani@users.noreply.github.com>
2023-03-29 03:34:04 +0200

    prints an hexgecimal number

--- print_HEX.c
+++ print_HEX.c
@@ -1,90 +1,36 @@
 #include "main.h"
 /**
- * printf_int - prints integer
- * @args: argument to print
- * Return: number of characters printed
+ * printf_HEX - prints an hexgecimal number.
+ * @val: arguments.
+ * Return: counter.
  */
-int printf_int(va_list args)
+int printf_HEX(va_list val)
 {
-	int n = va_arg(args, int);
-	int num, last = n % 10, digit, exp = 1;
-	int  i = 1;
+	int i;
+	int *array;
+	int counter = 0;
+	unsigned int num = va_arg(val, unsigned int);
+	unsigned int temp = num;
 
-	n = n / 10;
-	num = n;
-
-	if (last < 0)
+	while (num / 16 != 0)
 	{
-		_putchar('-');
-		num = -num;
-		n = -n;
-		last = -last;
-		i++;
+		num /= 16;
+		counter++;
 	}
-	if (num > 0)
+	counter++;
+	array = malloc(counter * sizeof(int));
+
+	for (i = 0; i < counter; i++)
 	{
-		while (num / 10 != 0)
-		{
-			exp = exp * 10;
-			num = num / 10;
-		}
-		num = n;
-		while (exp > 0)
-		{
-			digit = num / exp;
-			_putchar(digit + '0');
-			num = num - (digit * exp);
-			exp = exp / 10;
-			i++;
-		}
+		array[i] = temp % 16;
+		temp /= 16;
 	}
-	_putchar(last + '0');
-
-	return (i);
-}
-
-/**
- * printf_dec - prints decimal
- * @args: argument to print
- * Return: number of characters printed
- */
-
-int printf_dec(va_list args)
-{
-	int n = va_arg(args, int);
-	int num, last = n % 10, digit;
-	int  i = 1;
-	int exp = 1;
-
-	n = n / 10;
-	num = n;
-
-	if (last < 0)
+	for (i = counter - 1; i >= 0; i--)
 	{
-		_putchar('-');
-		num = -num;
-		n = -n;
-		last = -last;
-		i++;
+		if (array[i] > 9)
+			array[i] = array[i] + 7;
+		_putchar(array[i] + '0');
 	}
-	if (num > 0)
-	{
-		while (num / 10 != 0)
-		{
-			exp = exp * 10;
-			num = num / 10;
-		}
-		num = n;
-		while (exp > 0)
-		{
-			digit = num / exp;
-			_putchar(digit + '0');
-			num = num - (digit * exp);
-			exp = exp / 10;
-			i++;
-		}
-	}
-	_putchar(last + '0');
-
-	return (i);
+	free(array);
+	return (counter);
 }
