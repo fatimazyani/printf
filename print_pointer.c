d37143ee94117767cb8359eeaa4731db8cb70073
fatimazyani <123373140+fatimazyani@users.noreply.github.com>
2023-03-29 03:50:08 +0200

    print a pointer

--- print_pointer.c
+++ print_pointer.c
@@ -1,90 +1,30 @@
 #include "main.h"
 /**
- * printf_int - prints integer
- * @args: argument to print
- * Return: number of characters printed
+ * printf_pointer - prints a pointer.
+ * @val: arguments.
+ * Return: counter.
  */
-int printf_int(va_list args)
+int printf_pointer(va_list val)
 {
-	int n = va_arg(args, int);
-	int num, last = n % 10, digit, exp = 1;
-	int  i = 1;
+	void *p;
+	char *s = "(nil)";
+	long int a;
+	int b;
+	int i;
 
-	n = n / 10;
-	num = n;
-
-	if (last < 0)
+	p = va_arg(val, void*);
+	if (p == NULL)
 	{
-		_putchar('-');
-		num = -num;
-		n = -n;
-		last = -last;
-		i++;
-	}
-	if (num > 0)
-	{
-		while (num / 10 != 0)
+		for (i = 0; s[i] != '\0'; i++)
 		{
-			exp = exp * 10;
-			num = num / 10;
+			_putchar(s[i]);
 		}
-		num = n;
-		while (exp > 0)
-		{
-			digit = num / exp;
-			_putchar(digit + '0');
-			num = num - (digit * exp);
-			exp = exp / 10;
-			i++;
-		}
+		return (i);
 	}
-	_putchar(last + '0');
 
-	return (i);
-}
-
-/**
- * printf_dec - prints decimal
- * @args: argument to print
- * Return: number of characters printed
- */
-
-int printf_dec(va_list args)
-{
-	int n = va_arg(args, int);
-	int num, last = n % 10, digit;
-	int  i = 1;
-	int exp = 1;
-
-	n = n / 10;
-	num = n;
-
-	if (last < 0)
-	{
-		_putchar('-');
-		num = -num;
-		n = -n;
-		last = -last;
-		i++;
-	}
-	if (num > 0)
-	{
-		while (num / 10 != 0)
-		{
-			exp = exp * 10;
-			num = num / 10;
-		}
-		num = n;
-		while (exp > 0)
-		{
-			digit = num / exp;
-			_putchar(digit + '0');
-			num = num - (digit * exp);
-			exp = exp / 10;
-			i++;
-		}
-	}
-	_putchar(last + '0');
-
-	return (i);
+	a = (unsigned long int)p;
+	_putchar('0');
+	_putchar('x');
+	b = printf_hex_aux(a);
+	return (b + 2);
 }
